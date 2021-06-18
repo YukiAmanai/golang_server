ce1f9a26f7ebd95350cb7962caa3095562524602
YukiAmanai <yuki_amanai@icloud.com>
2021-06-18 11:01:14 +0900

    fix

---
+++ server1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	message := []byte("Hello World!")
+	// Add message to the response
+	_, err := writer.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	// register hander
+	http.HandleFunc("/", viewHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
